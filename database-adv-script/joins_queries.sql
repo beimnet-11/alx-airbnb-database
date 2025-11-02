002fcfca96045b41cbef60fe63e575c15a5b08dc
Beimnet Abera <beimnet.mi1pa@gmail.com>
2025-11-02 23:20:41 +0300

    Add SQL queries for different types of joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -71,3 +71,56 @@ FROM
 FULL OUTER JOIN 
     bookings b ON u.user_id = b.user_id;
 */
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT 
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date,
+    u.id AS user_id,
+    u.email,
+    u.name
+FROM 
+    bookings b
+INNER JOIN 
+    users u ON b.user_id = u.id;
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    p.id AS property_id,
+    p.title,
+    p.description,
+    r.id AS review_id,
+    r.rating,
+    r.comment
+FROM 
+    properties p
+LEFT JOIN 
+    reviews r ON p.id = r.property_id;
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+-- Using UNION to simulate FULL OUTER JOIN in MySQL
+SELECT 
+    u.id AS user_id,
+    u.email,
+    u.name,
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+LEFT JOIN 
+    bookings b ON u.id = b.user_id
+
+UNION
+
+SELECT 
+    u.id AS user_id,
+    u.email,
+    u.name,
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+RIGHT JOIN 
+    bookings b ON u.id = b.user_id;
